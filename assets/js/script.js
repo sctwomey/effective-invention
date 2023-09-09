b9da950c44c822dfcc693e7e179328d9502a66d8
sctwomey <sctwomey@gmail.com>
2023-09-09 13:54:16 -0700

    Add variables to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getPasswordOptions {
+  var passwordLength = prompt("How many characters do you want for your password?")
+  var hasLowerCaseLetters = prompt("Do you want lowercase letters in your password?")
+  var hasUpperCaseLetters = prompt("Do you want uppercase letters in your password?")
+  var hasSpecialCharacters = prompt("Do you want any special characters in your password?")
+}
+
+function generatePassword() {
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
