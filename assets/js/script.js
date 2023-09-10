82e0014175d17d55cb4df563dd9f4aa17423bff8
sctwomey <sctwomey@gmail.com>
2023-09-10 13:29:21 -0700

    Add prompts and confirms for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,29 @@ var symbolCharacters = "!@#$%&*()+={}[]/';:";
 
 function generatePassword() {
 
+  // var passwordLength = Number(prompt("How many characters do you want for your password? Please choose a value between 8 and 128."));
+  var passwordLength = Number(prompt("How many characters do you want for your password? Please choose a value between 8 and 128."));
+
+  if (!passwordLength) {
+    // If the user presses cancel, immediately ends function.
+    return;
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    // Checks for correct user input, and informs user if it is incorrect.
+    passwordLength = prompt("Please enter a valid number!");
+  };
+
+  // Validates whether a user wants numbers.
+  var hasNumbers = confirm("Do you want numbers in your password? If so, click OK! If not, click Cancel!");
+
+  // Validates whether a user wants lowercase letters.
+  var hasLowerCaseLetters = confirm("Do you want lowercase letters in your password? If so, click OK! If not, click Cancel!")
+
+  // Validates whether a user wants uppercase letters.
+  var hasUpperCaseLetters = confirm("Do you want uppercase letters in your password? If so, click OK! If not, click Cancel!");
+
+  // Validates whether a user wants symbols.
+  var hasSymbolCharacters = confirm("Do you want any symbols in your password? If so, click OK! If not, click Cancel!");
+
 }
 
 // Write password to the #password input
